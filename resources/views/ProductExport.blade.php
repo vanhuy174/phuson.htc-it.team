cdd5e1a5ca97dfb05b886426b2fc80ee820e5a4d
vanhuy174 <becodon174@gmail.com>
2020-06-08 09:59:50 +0700

    edit temp export excel

--- resources/views/ProductExport.blade.php
+++ resources/views/ProductExport.blade.php
@@ -1,44 +1,42 @@
 <table>
-    <div class="row" style="text-align: center">
-        <tr class="col-md-6">
-            <th>Trại giam Phú Sơn 4</th>
-            <th></th>
-            <th>BẢNG KÊ NHẬP HÀNG TƯƠI SỐNG</th>
-        </tr>
-        <tr class="col-md-6">
-            <th>Đội Hậu Cần - Tài Vụ</th>
-            <th></th>
-            <th>Tháng: {{$date}}</th>
-        </tr>
+    <tr>
+        <th colspan="3" style="text-align: center; font-weight: bold;">Trại giam Phú Sơn 4</th>
+        <th colspan="4" style="text-align: center; font-weight: bold;">BẢNG KÊ NHẬP HÀNG TƯƠI SỐNG</th>
+    </tr>
+    <tr>
+        <th colspan="3" style="text-align: center; font-weight: bold;">Đội Hậu Cần - Tài Vụ</th>
+        <th colspan="4" style="text-align: center; font-weight: bold;">Tháng: {{$date}}</th>
+    </tr>
+    <tr>
+        <th colspan="3" style="text-align: center; font-weight: bold;"></th>
+        <th colspan="4" style="text-align: center; font-weight: bold;">Phân trại số: {{$area->name}}</th>
+    </tr>
+</table>
+<table class="table">
+    <tr>
+        <th style="border: 2px solid #000000; width: 10px;">Ngày</th>
+        <th style="border: 2px solid #000000; width: 20px;">Tên hàng hóa</th>
+        <th style="border: 2px solid #000000; width: 7px;">ĐVT</th>
+        <th style="border: 2px solid #000000; width: 8px;">Số lượng</th>
+        <th style="border: 2px solid #000000; width: 12px;">Đơn giá</th>
+        <th style="border: 2px solid #000000; width: 13px;">Thành tiền</th>
+        <th style="border: 2px solid #000000; width: 10px;">Ghi chú</th>
+    </tr>
+    @foreach($products as $product)
         <tr>
-            <th>Phân trại số: {{$area->name}}</th>
+            <td style="border: 2px solid #000000;">{{$product->created_at}}</td>
+            <td style="border: 2px solid #000000;">{{$product->name}}</td>
+            <td style="border: 2px solid #000000;">{{$product->type_caculating}}</td>
+            <td style="border: 2px solid #000000;">{{$product->total}}</td>
+            <td style="border: 2px solid #000000;">{{$product->price}}</td>
+            <td style="border: 2px solid #000000;">{{$product->total_price}}</td>
+            <td style="border: 2px solid #000000;">{{$product->note}}</td>
         </tr>
-        <tr></tr>
-        <tr>
-            <th>Ngày</th>
-            <th>Tên hàng hóa</th>
-            <th>ĐVT</th>
-            <th>Số lượng</th>
-            <th>Đơn giá</th>
-            <th>Thành tiền</th>
-            <th>Ghi chú</th>
-        </tr>
-        @foreach($products as $product)
-            <tr>
-                <td>{{$product->created_at}}</td>
-                <td>{{$product->name}}</td>
-                <td>{{$product->type_caculating}}</td>
-                <td>{{$product->total}}</td>
-                <td>{{$product->price}}</td>
-                <td>{{$product->total_price}}</td>
-                <td>{{$product->note}}</td>
-            </tr>
-        @endforeach
-        <tr></tr>
-        <tr class="p-5 row">
-            <th>Người nhận</th>
-            <th></th>
-            <th>Người giao</th>
-        </tr>
-    </div>
+    @endforeach
+</table>
+<table>
+    <tr>
+        <th colspan="3" style="text-align: center; font-weight: bold;">Người nhận</th>
+        <th colspan="4" style="text-align: center; font-weight: bold;">Người giao</th>
+    </tr>
 </table>
